384ce7a44db5281fa397578fdac7e02be3d72abc
Nikolay <tifilum74@mail.ru>
2021-02-25 09:33:52 +0300

    добавлена валидация для обновления сотрудника

---
+++ app/Http/Requests/UpdateStaffRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateStaffRequest extends ApiRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'string|between:2,50',
+            'middle_name' => 'string|between:2,50',
+            'last_name' => 'string|between:2,50',
+            'birthday' => 'date_format:d.m.Y|max:20|after:1950-01-01',
+            'job_id' => 'exists:states,_without:job_string',
+            'job_string' => '_without:job_id'
+        ];
+    }
+}
